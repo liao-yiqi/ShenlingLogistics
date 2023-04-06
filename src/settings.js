867db18e32c32f1d42801cc345000563573d3d98
komorebiHl <821890952@qq.com>
2023-04-06 22:40:03 +0800

    feat:开发前清理完成

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '神领TMS管理系统',
 
   /**
    * @type {boolean} true | false
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,5 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
+  device: state => state.app.device
 }
 export default getters
