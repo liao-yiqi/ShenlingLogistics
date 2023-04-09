426e65cc9bf0d89d167084b7a3be9a370367a2c9
HL <821890952@qq.com>
2023-04-09 22:00:10 +0800

    perf:将基地址放置到development文件中

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: 'https://slwl-api.itheima.net/manager', // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // 超时时间
 }) // 创建一个axios的实例
