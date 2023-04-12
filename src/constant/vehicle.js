ff59c574da829e1a0472d495f69464a3272a1323
HL <821890952@qq.com>
2023-04-12 21:14:28 +0800

    feat:新增枚举文件

---
+++ src/constant/vehicle.js
@@ -0,0 +1,22 @@
+export default {
+  // 车载重量
+  vehicleWeight: [
+    { id: 'RANGE_LEVEL_1', value: '<1.8(吨)' },
+    { id: 'RANGE_LEVEL_2', value: '1.8-6(吨)' },
+    { id: 'RANGE_LEVEL_3', value: '6-14(吨)' },
+    { id: 'RANGE_LEVEL_4', value: '14-30(吨)' },
+    { id: 'RANGE_LEVEL_5', value: '30-50(吨)' },
+    { id: 'RANGE_LEVEL_6', value: '50-100(吨)' },
+    { id: 'RANGE_LEVEL_7', value: '>100(吨)' }
+  ],
+  // 车载体积
+  vehiclVolume: [
+    { id: 'RANGE_LEVEL_1', value: '<3(m³)' },
+    { id: 'RANGE_LEVEL_2', value: '3-5(m³)' },
+    { id: 'RANGE_LEVEL_3', value: '5-10(m³)' },
+    { id: 'RANGE_LEVEL_4', value: '10-15(m³)' },
+    { id: 'RANGE_LEVEL_5', value: '15-30(m³)' },
+    { id: 'RANGE_LEVEL_6', value: '30-50(m³)' },
+    { id: 'RANGE_LEVEL_7', value: '50-80(m³)' }
+  ]
+}
